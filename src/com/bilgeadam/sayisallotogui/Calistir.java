d7e003720797fa41d57201bc8223f0f0d4290a54
mert <mert.baskaya07@gmail.com>
2021-11-28 14:29:46 +0300

    Initial commit

---
+++ src/com/bilgeadam/sayisallotogui/Calistir.java
@@ -0,0 +1,14 @@
+package com.bilgeadam.sayisallotogui;
+
+import ekran.SayisalLoto;
+
+public class Calistir {
+    public static void main(String[] args) {
+
+
+        SayisalLoto loto = new SayisalLoto();
+        loto.setVisible(true);
+    }
+
+
+}
